5fa7e9697f1433ef83e99ce2a6d01803d6d5330e
顏烈彬[子鳴] <slbyan@gmail.com>
2013-12-13 12:12:15 +0800

    and overloading functions; format codes; remove some inlines

--- source/ai-primitive.adb
+++ source/ai-primitive.adb
@@ -7,127 +7,215 @@ package body ai.primitive is
       m : real := a (b (b'first));
    begin
       for c of b loop
-           if a(c) > m then
-               m := a(c);
-           end if;
-       end loop;
-       return m;
+         if a (c) > m then
+            m := a (c);
+         end if;
+      end loop;
+      return m;
+   end max;
+
+   function max (a : real_array) return real is
+      m : real := a (a'first);
+   begin
+      for c of a loop
+         if c > m then
+            m := c;
+         end if;
+      end loop;
+      return m;
    end max;
 
    function min (a : real_array; b : index_array) return real is
-       m : real := a(b(b'first));
+      m : real := a (b (b'first));
    begin
-       for c of b loop
-          if a(c) < m then
-             m := a(c);
-          end if;
-       end loop;
-       return m;
+      for c of b loop
+         if a (c) < m then
+            m := a (c);
+         end if;
+      end loop;
+      return m;
    end min;
 
-   -- TODO Check b's values
-   function mean (a : real_array; b : index_array) return real is
-       sum : real := 0.0;
+   function min (a : real_array) return real is
+      m : real := a (a'first);
    begin
-       for c of b loop
-           sum := sum + a(c);
-       end loop;
-       sum := sum / real(b'length);
-       return sum;
+      for c of a loop
+         if c < m then
+            m := c;
+         end if;
+      end loop;
+      return m;
+   end min;
+
+   function mean (a : real_array; b : index_array) return real is
+      m : real := 0.0;
+   begin
+      for c of b loop
+         m := m + a (c);
+      end loop;
+      m := m / real (b'length);
+      return m;
    end mean;
 
-   function variance(a : real_array; b : index_array) return real is
-       v, m: real := 0.0;
+   function mean (a : real_array) return real is
+      m : real := 0.0;
    begin
-       for c of b loop
-           m := m + a(c);
-       end loop;
-       m := m / real(b'length);
-       for c of b loop
-           v := v + (a(c)-m)**2;
-       end loop;
-       return v/real(b'length); -- should we apply  bessel's correction (-1)?
+      for c of a loop
+         m := m + c;
+      end loop;
+      m := m / real (a'length);
+      return m;
+   end mean;
+
+   -- E(x^2) - E(x)^2
+   function variance(a : real_array; b : index_array) return real is
+      v, m : real := 0.0;
+   begin
+      for c of b loop
+         m := m + a (c);
+         v := v + a (c) * a (c);
+      end loop;
+      m := m / real (b'length);
+      v := v / real (b'length) - m * m;
+      return v; -- N.B. No bessel's correction (N-1) applied!
+   end variance;
+
+   function variance(a : real_array) return real is
+      v, m : real := 0.0;
+   begin
+      for c of a loop
+         m := m + c;
+         v := v + c * c;
+      end loop;
+      m := m / real (a'length);
+      v := v / real (a'length) - m * m;
+      return v; -- N.B. No bessel's correction (N-1) applied!
    end variance;
 
    function standard_deviation (a : real_array; b : index_array) return real is
    begin
-       return sqrt(variance(a,b));
+      return sqrt(variance(a,b));
    end standard_deviation;
 
-   function central_moment (a : real_array;
-       b : index_array;
-       n : positive) return real is
-       m : real := mean(a,b);
-       v : real := 0.0;
+   function standard_deviation (a : real_array) return real is
    begin
-       for c of b loop
-           v := v + (a(c)-m)**n;
-       end loop;
-       return v/real(b'length);
+      return sqrt(variance(a));
+   end standard_deviation;
+
+   function central_moment (a : real_array; b : index_array; n : positive)
+      return real is
+      m : real := mean (a,b);
+      v : real := 0.0;
+   begin
+      for c of b loop
+         v := v + (a (c) - m) ** n;
+      end loop;
+      return v / real (b'length);
    end central_moment;
 
-   -- TODO avoid computing mean twice
-   function normalized_moment (a : real_array;
-       b : index_array;
-       n : positive) return real is
+   function central_moment (a : real_array; n : positive) return real is
+      m : real := mean (a);
+      v : real := 0.0;
    begin
-       return central_moment(a,b,n) / (standard_deviation(a,b)**n);
+      for c of a loop
+         v := v + (c - m) ** n;
+      end loop;
+      return v / real (a'length);
+   end central_moment;
+
+   function normalized_moment (a : real_array; b : index_array; n : positive)
+      return real is
+      m  : real := 0.0;
+      v2 : real := 0.0;
+      vn : real := 0.0;
+   begin
+      for c of b loop
+         m  := m + a (c);
+      end loop;
+      m := m / real(b'length);
+      for c of b loop
+         v2 := v2 + (a (c) - m) * (a (c) - m);
+         vn := vn + (a (c) - m) ** n;
+      end loop;
+      v2 := v2 / real (b'length);
+      v2 := sqrt (v2) ** n;
+      vn := vn / real (b'length);
+      return vn / v2;
+   end normalized_moment;
+
+   function normalized_moment (a : real_array; n : positive) return real is
+      m  : real := 0.0;
+      v2 : real := 0.0;
+      vn : real := 0.0;
+   begin
+      for c of a loop
+         m  := m + c;
+      end loop;
+      m := m / real (a'length);
+
+      for c of a loop
+         v2 := v2 + (c - m) * (c - m);
+         vn := vn + (c - m) ** n;
+      end loop;
+
+      v2 := v2 / real (a'length);
+      v2 := sqrt (v2) ** n;
+      vn := vn / real (a'length);
+      return vn / v2;
    end normalized_moment;
 
    ---------------
    -- Distances --
    ---------------
 
-   function squared_euclidean_distance(a,b:real_array) return real is
-       sed : real := 0.0;
-       item_b : real;
+   function squared_euclidean_distance(a, b : real_array) return real is
+      m : real := 0.0;
    begin
-       for j in a'range loop
-           item_b := b(j+b'first-a'first);
-           sed := sed + (a(j)-item_b)**2;
-       end loop;
-       return sed;
+      for j in a'range loop
+         m := m + (a (j) - b (j + b'first - a'first)) ** 2;
+      end loop;
+      return m;
    end squared_euclidean_distance;
 
-   function euclidean_distance(a,b:real_array) return real is
+   function euclidean_distance(a, b : real_array) return real is
    begin
-       return sqrt(squared_euclidean_distance(a,b));
+      return sqrt(squared_euclidean_distance(a,b));
    end euclidean_distance;
 
-   function manhattan_distance(a,b:real_array) return real is
-       md : real := 0.0;
+   function manhattan_distance(a, b : real_array) return real is
+      m : real := 0.0;
    begin
-       for j in a'range loop
-           md := md + abs(a(j) - b(j+b'first-a'first));
-       end loop;
-       return md;
+      for j in a'range loop
+         m := m + abs (a (j) - b (j + b'first - a'first));
+      end loop;
+      return m;
    end manhattan_distance;
 
-   function sup_distance(a,b:real_array) return real is
-       distance: real := 0.0;
-       diff : real;
+   function sup_distance(a, b : real_array) return real is
+      m : real := 0.0;
+      d : real;
    begin
-       for j in a'range loop
-           diff := abs(a(j) - b(j+b'first-a'first));
-           if diff > distance then
-               distance := diff;
-           end if;
-       end loop;
-       return distance;
+      for j in a'range loop
+         d := abs (a (j) - b (j + b'first - a'first));
+         if d > m then
+            m := d;
+         end if;
+      end loop;
+      return m;
    end sup_distance;
 
-   function cosine_distance(a,b:real_array) return real is
-       dot_prod, norm_a, norm_b: real := 0.0;
+   function cosine_distance(a, b : real_array) return real is
+      m, na, nb: real := 0.0;
    begin
-       for j in a'range loop
-           dot_prod := dot_prod +  a(j) * b(j+b'first-a'first);
-           norm_b := norm_a + a(j) ** 2;
-           norm_b := norm_b + b(j) ** 2;
-       end loop;
+      for j in a'range loop
+         m  := m  + a (j) * b (j + b'first - a'first);
+         na := na + a (j) * a (j);
+         nb := nb + b (j) * b (j);
+      end loop;
 
-       if dot_prod = 0.0 then return 1.0; end if;
+      if m = 0.0 then return 1.0; end if;
 
-       return 1.0 - dot_prod / sqrt(norm_a*norm_b);
+      return 1.0 - m / sqrt (na * nb);
    end cosine_distance;
 
 end ai.primitive;
--- source/ai-primitive.ads
+++ source/ai-primitive.ads
@@ -1,37 +1,40 @@
 pragma ada_12;
+
 with ada.numerics.generic_elementary_functions;
 package ai.primitive is
    type real is new long_long_float;
-   type index is new long_long_integer;
+   type index is new integer;
    type real_array is array (index range <>) of real;
    type index_array is array (index range <>) of index;
 
+   -- Math functions for real
    package elementary_functions is new
-      ada.numerics.generic_elementary_functions (
-      real);
+   ada.numerics.generic_elementary_functions (real);
 
-   error_mismatch_index : exception;
+   index_mismatched : exception;
 
    function max (a : real_array; b : index_array) return real with inline;
-    function min (a : real_array; b : index_array) return real with inline;
+   function max (a : real_array) return real with inline;
+   function min (a : real_array; b : index_array) return real with inline;
+   function min (a : real_array) return real with inline;
    ----------------
    -- Statistics --
    ----------------
+
    -- 1st raw moment
-   function mean (a : real_array; b : index_array) return real with inline;
+   function mean (a : real_array; b : index_array) return real;
 
    -- 2nd central moment
-   function variance (a : real_array; b : index_array) return real with inline;
+   function variance (a : real_array; b : index_array) return real;
 
-   function standard_deviation (a : real_array;  b : index_array)
-   return real with inline;
-   
-   function central_moment (a : real_array; b : index_array; n : positive)
-    return real with inline;
+   function standard_deviation (a : real_array; b : index_array) return real;
 
-   function normalized_moment (a : real_array; b : index_array; n : positive)
-   return real with inline;
-    -- mode
+   function central_moment (a : real_array; b : index_array; n : positive) return real;
+
+   function normalized_moment (a : real_array; b : index_array; n : positive) return real;
+
+   function normalized_moment (a : real_array; n : positive) return real;
+   -- mode
 
    ---------------
    -- Distances --
@@ -46,7 +49,7 @@ package ai.primitive is
 
    function cosine_distance(a,b:real_array) return real with inline;
 
--- hamming_distance
--- simple_matching_distance
+   -- hamming_distance
+   -- simple_matching_distance
 
 end ai.primitive;
