cb279d38f6a2bb5126062e5b908398b3d0cac577
顏烈彬[子鳴] <slbyan@gmail.com>
2014-01-14 13:06:26 +0800

    update naivebayes test

--- tests/naivebayes_driver.adb
+++ tests/naivebayes_driver.adb
@@ -1,74 +1,58 @@
-with ML.Classification.Naivebayes;
-with Ada.Containers.Vectors;
+pragma Ada_2012;
 with Ada.Text_IO;
-procedure naivebayes_driver is
-   type Real is new Long_Float;  --  Long_Long_Float;
-   package AF is new  Ada.Text_IO.Float_IO (Real);
+with ML.Classification.Naivebayes.Classifier;
+with ML.Classification.Naivebayes.Prioris;
+procedure Naivebayes_Driver is
+   use ML.Classification.Naivebayes;
+   type Dim_Type is (Sepal_Length, Sepal_Width, Petal_Length, Petal_Width);
+   type Cat_Type is (Setosa, Versicolor, Virginica);
+   package IRIS is new Classifier (Dim_Type, Cat_type);
+   use IRIS;
+
    package TIO renames Ada.Text_IO;
-   type My_Feature is (Sepal_Length, Sepal_Width, Petal_Length, Petal_Width);
-   type My_Class is (Setosa, Versicolor, Virginica);
-   type My_Array is array (My_Feature) of Real;
-   package My_Array_Vector_Package is new
-   Ada.Containers.Vectors (Positive, My_Array);
-   use My_Array_Vector_Package;
+   package AF is new  Ada.Text_IO.Float_IO (Long_Float);
+   package AE is new  Ada.Text_IO.Enumeration_IO (Cat_Type);
 
-   data : Vector;
-
-   function Length return Natural;
-   function Element (x : Positive) return My_Array;
-   function Belong (x : Positive) return My_Class;
-
-   function Length return Natural is
+   function To (SL, SW, PL, PW : Long_Float) return Cell_Array;
+   function To (SL, SW, PL, PW : Long_Float) return Cell_Array is
    begin
-      if data.Is_Empty then
-         return 0;
-      end if;
-      return Natural (data.Length);
-   end Length;
+      return Cell_Array'(
+         new Prioris.Long_Float_Variable'(V => SL),
+         new Prioris.Long_Float_Variable'(V => SW),
+         new Prioris.Long_Float_Variable'(V => PL),
+         new Prioris.Long_Float_Variable'(V => PW));
+   end To;
 
-   function Element (x : Positive) return My_Array is
-   begin
-      return My_Array '(data.Element (x));
-   end Element;
+   F_1 : My_Feature;
+   sl, sw, pl, pw : Long_Float;
 
-   function Belong (x : Positive) return My_Class is
-   begin
-      if x <= 50 then
-         return Setosa;
-      end if;
-      if x > 100 then
-         return Virginica;
-      end if;
-      return Versicolor;
-   end Belong;
-
-   package NB is new ML.Classification.Naivebayes
-      (Real, My_Feature, My_Class, My_Array, Length, Belong, Element);
-
-   NB_Obj : NB.Object;
    file : TIO.File_Type;
-   f : My_Array;
-   x : Real;
+   s    : Cat_Type;
+
+   E_1 : My_Event := (To (6.2, 3.4, 5.4, 2.3), Setosa); --  was Virginica);
+   E_2 : My_Event := (To (5.9, 3.0, 5.1, 1.8), Versicolor); --  was Virginica);
 begin
+   -- Setup
+   F_1.F := (others => (others => new Prioris.Normal_Priori));
+   
+   --  Train
    TIO.Open
-      (File => file, Mode => TIO.In_File, Name => "iris.t");
+      (File => file, Mode => TIO.In_File, Name => "iris.tab");
    Process :
    while not TIO.End_Of_File (file) loop
-      for c in My_Feature loop
-         AF.Get (file, x);
-         f (c) := x;
-      end loop;
-      data.Append (f);
+      AF.Get (file, sl);
+      AF.Get (file, sw);
+      AF.Get (file, pl);
+      AF.Get (file, pw);
+      AE.Get (file, s);
+      F_1.Train (My_Event'(To (sl, sw, pl, pw), s));
    end loop Process;
    TIO.Close (file);
-   NB_Obj.Train;
-   NB_Obj.Put;
-   NB_Obj.Predict (Element (52));
-   NB_Obj.Predict (Element (52), ML.Log_Normal);
-   TIO.New_Line;
-   for c of data loop
-      TIO.Put (NB_Obj.Predict (c)'Img & "   ");
-   end loop;
-exception
-   when TIO.End_Error => null; --  In case of blank lines
-end naivebayes_driver;
+   --  Done
+   F_1.Done;
+   --  Predict
+   F_1.Predict (E_1);
+   F_1.Predict (E_2);
+   Ada.Text_IO.Put_Line (E_1.C'Img);
+   Ada.Text_IO.Put_Line (E_2.C'Img);
+end Naivebayes_Driver;
