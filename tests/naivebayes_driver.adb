f5c84ba1ad8d220d87f2ed5393fc761427314549
顏烈彬[子鳴] <slbyan@gmail.com>
2014-01-14 14:19:44 +0800

    update Naivebayes test

--- tests/naivebayes_driver.adb
+++ tests/naivebayes_driver.adb
@@ -32,13 +32,13 @@ procedure Naivebayes_Driver is
    E_1 : My_Event := (To (6.2, 3.4, 5.4, 2.3), Setosa); --  was Virginica);
    E_2 : My_Event := (To (5.9, 3.0, 5.1, 1.8), Versicolor); --  was Virginica);
 begin
-   -- Setup
+   --  Setup
    F_1.F := (others => (others => new Prioris.Normal_Priori));
-   
+
    --  Train
    TIO.Open
       (File => file, Mode => TIO.In_File, Name => "iris.tab");
-   Process :
+   Train :
    while not TIO.End_Of_File (file) loop
       AF.Get (file, sl);
       AF.Get (file, sw);
@@ -46,13 +46,28 @@ begin
       AF.Get (file, pw);
       AE.Get (file, s);
       F_1.Train (My_Event'(To (sl, sw, pl, pw), s));
-   end loop Process;
+   end loop Train;
    TIO.Close (file);
+
    --  Done
    F_1.Done;
    --  Predict
-   F_1.Predict (E_1);
-   F_1.Predict (E_2);
-   Ada.Text_IO.Put_Line (E_1.C'Img);
-   Ada.Text_IO.Put_Line (E_2.C'Img);
+
+   TIO.Open
+      (File => file, Mode => TIO.In_File, Name => "iris.tab");
+   Predict :
+   while not TIO.End_Of_File (file) loop
+      AF.Get (file, sl);
+      AF.Get (file, sw);
+      AF.Get (file, pl);
+      AF.Get (file, pw);
+      AE.Get (file, s);
+      declare
+         e : My_Event := (To (sl, sw, pl, pw), s);
+      begin
+         F_1.Predict (e);
+         Ada.Text_IO.Put_Line (e.C'Img);
+      end;
+   end loop Predict;
+   TIO.Close (file);
 end Naivebayes_Driver;
